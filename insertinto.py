e9285cb50965f8573db66dde705a192e8acbdd32
Higo Ribeiro <higoribeiro0@gmail.com>
2023-04-10 21:28:38 -0300

    INSERT INTO

---
+++ insertinto.py
@@ -0,0 +1,29 @@
+import sqlite3 
+from sqlite3 import Error
+
+###Criar conexao
+def ConexaoBanco():
+    caminho="D:\\Python\\Aulas\\Banco\\agenda.db"
+    con=None
+    try:
+        con=sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
+
+vcon=ConexaoBanco()
+
+nome=input("Digite o seu nome: ")
+telefone=input("Digite o telefone: ")
+email=input("Digite o seu email: ")
+
+vsql="INSERT INTO tb_contatos(T_NOMECONTATO, T_TELEFONECONTATO, T_EMAILCONTATO)VALUES('"+nome+"','"+telefone+"','"+email+"')"
+
+def insert(conexao,sql):
+    try: 
+        c=conexao.cursor()
+        c.execute(sql)
+        conexao.commit()
+        print("Registro inserido")
+    except Error as ex:
+        print(ex)
